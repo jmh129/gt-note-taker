2360f528eb3a88b666cd3ac447b068d4d13b9238
James Holding <james.holding90@gmail.com>
2020-07-07 21:02:51 -0400

    fixed delete issue.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,13 +8,17 @@ module.exports = (app) => {
     res.json(notesDb);
   });
 
+  //   Worked with TA for these.
   // * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
-  //   Had help with TA for this.
+  // Setting Post Route
   app.post("/api/notes", (req, res) => {
     let newNote = req.body;
     console.log(newNote);
     notesDb.push(newNote);
+    // Adds the ID # for each note.
+    notesDb.forEach((note, i) => (note.id = ++i));
+    // Writes the file
     fs.writeFile(
       "./db/db.json",
       JSON.stringify(notesDb),
@@ -28,5 +32,31 @@ module.exports = (app) => {
       }
     );
   });
+
+  // * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
+  // Setting Delete Route
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err) => {
+      if (err) {
+        console.log("Error!");
+      }
+      // Using .filter() to remove the deleted note from list.
+      // This is used to create a list that only contains items that do not have the matching id.
+      notesDb = notesDb.filter(
+        (note) => parseInt(note.id) !== parseInt(req.params.id)
+      );
+      fs.writeFile(
+        __dirname + "/../db/db.json",
+        JSON.stringify(notesDb),
+        (err) => {
+          if (err) throw err;
+          else {
+            res.json(req.params.id);
+            console.log("Deleted note successfully.");
+          }
+        }
+      );
+    });
+  });
 };
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
